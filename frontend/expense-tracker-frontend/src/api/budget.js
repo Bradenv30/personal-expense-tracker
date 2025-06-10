edf9a77fb316cf0eea1871e39dded47f71f8473b
Braden Vail <bradenvail30@gmail.com>
2025-06-09 22:42:37 -0400

    Refactored budget API to return res.data only

--- frontend/expense-tracker-frontend/src/api/budget.js
+++ frontend/expense-tracker-frontend/src/api/budget.js
@@ -1,21 +1,21 @@
 import axios from "./axios";
 
 export function getAllBudgets() {
-  return axios.get("/budget");
+  return axios.get("/budget").then((res) => res.data);
 }
 
 export function getBudgetById(id) {
-  return axios.get(`/budget/${id}`);
+  return axios.get(`/budget/${id}`).then((res) => res.data);
 }
 
 export function createBudget(data) {
-  return axios.post("/budget", data);
+  return axios.post("/budget", data).then((res) => res.data);
 }
 
 export function updateBudget(id, data) {
-  return axios.patch(`/budget/${id}`, data);
+  return axios.patch(`/budget/${id}`, data).then((res) => res.data);
 }
 
 export function deleteBudget(id) {
-  return axios.delete(`/budget/${id}`);
+  return axios.delete(`/budget/${id}`).then((res) => res.data);
 }
