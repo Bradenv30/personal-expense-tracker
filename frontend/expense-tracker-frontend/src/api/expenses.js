a0cccb1c72f60874a668781664b4103ec71f5c6e
Braden Vail <bradenvail30@gmail.com>
2025-06-09 22:42:16 -0400

    Refactored expenses API to return res.data only

--- frontend/expense-tracker-frontend/src/api/expenses.js
+++ frontend/expense-tracker-frontend/src/api/expenses.js
@@ -1,21 +1,21 @@
 import axios from "./axios";
 
 export function getAllExpenses() {
-  return axios.get("/expenses");
+  return axios.get("/expenses").then((res) => res.data);
 }
 
 export function getExpenseById(id) {
-  return axios.get(`/expenses/${id}`);
+  return axios.get(`/expenses/${id}`).then((res) => res.data);
 }
 
 export function createExpense(data) {
-  return axios.post("/expenses", data);
+  return axios.post("/expenses", data).then((res) => res.data);
 }
 
 export function updateExpense(id, data) {
-  return axios.patch(`/expenses/${id}`, data);
+  return axios.patch(`/expenses/${id}`, data).then((res) => res.data);
 }
 
 export function deleteExpense(id) {
-  return axios.delete(`/expenses/${id}`);
+  return axios.delete(`/expenses/${id}`).then((res) => res.data);
 }
