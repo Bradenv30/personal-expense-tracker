f46d4ecd6272025b05558512ec7882f9e0a1a3ca
Braden Vail <bradenvail30@gmail.com>
2025-06-10 20:59:41 -0400

    Create User table with O-T-M relationships; added foreign keys to Expenses and Budget; updated Budget DateTime to use timezone

--- backend/model.py
+++ backend/model.py
@@ -1,16 +1,30 @@
-from sqlalchemy import CheckConstraint
+from sqlalchemy import CheckConstraint, func
 from datetime import datetime
 
 
 # Create tables for expenses and budget
 def init_models(db):
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(50), nullable=False, unique=True)
+        password = db.Column(db.String(50), nullable=False)
+        created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+        #OTM relationships
+        expenses = db.relationship("Expenses", back_populates="user")
+        budgets = db.relationship("Budget", back_populates="user")
+        
     class Expenses(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(15))
+        user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+        name = db.Column(db.String(30))
         amount = db.Column(db.Numeric(10,2), nullable=False)
         date = db.Column(db.Date)
         description = db.Column(db.Text)
         type = db.Column(db.String(50))
+        
+        #Relationship to User
+        user = db.relationship("User", back_populates="expenses")
     
         __table_args__ = (
             CheckConstraint("amount >= 0", name="check_amount_not_negative"),
@@ -22,61 +36,21 @@ def init_models(db):
     
     class Budget(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(15))
+        user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+        name = db.Column(db.String(30))
         amount = db.Column(db.Numeric(10,2), nullable=False)
         is_active = db.Column(db.Boolean, nullable=False, default=True)
         start_date = db.Column(db.Date)
         end_date = db.Column(db.Date)
-        created_at = db.Column(db.DateTime, default=datetime.utcnow)
-        updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+        created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+        updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+        
+        #Relationship to User
+        user = db.relationship("User", back_populates="budgets")
         
         __table_args__ = (
             CheckConstraint("amount >= 0", name="check_amount_not_negative"),
         )
     
-    return Expenses, Budget
+    return User, Expenses, Budget
 
-
-
-
-
-"""
-# Old code from app.py
-#Store data in variables for in memory testing
-expenses = []
-budget = {"monthly limit": 500}
-expenses_type = ["Subscription", "Travel/Transportation", "Food", "Entertainment", "Bills/Utilities", "Groceries/Neccesities"]
-expense1 = {
-    "id": 1,
-    "amount": 25,
-    "type": "Entertainment",
-    "description": "Went to the movies",
-    "date" : "05/25/2025",
-}
-expense2 = {
-    "id": 2,
-    "amount": 1200,
-    "type": "Bills/Utilities",
-    "description": "Paid rent",
-}
-expenses.append(expense1)
-expenses.append(expense2)
-
-#Set up home page route
-@app.route('/', methods=['GET'])
-def home():
-    return "<p>Hello!<p>"
-    
-#Create a route for 'budget' that displays the current values in budget
-@app.route('/budget', methods=['GET'])
-def get_budget():
-    return jsonify(budget)
-
-# Route that returns expenses list
-@app.route('/expenses', methods=['GET'])
-def get_expenses():
-    return jsonify(expenses)
-
-#end old code
-
-"""
