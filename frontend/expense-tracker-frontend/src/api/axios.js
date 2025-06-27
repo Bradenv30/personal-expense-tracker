341640b5da321191d0752f8c5d6529f0da6199ba
Braden Vail <bradenvail30@gmail.com>
2025-06-27 18:39:26 -0400

    Add JWT authorization header with request interceptor

--- frontend/expense-tracker-frontend/src/api/axios.js
+++ frontend/expense-tracker-frontend/src/api/axios.js
@@ -8,4 +8,12 @@ const instance = axios.create({
   withCredentials: false,
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default instance;
