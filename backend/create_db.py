a5f7fe5434e9043395922580e4193609731f2aa2
Braden Vail <bradenvail30@gmail.com>
2025-06-27 16:41:20 -0400

    Remove unnecessary imports

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,8 +1,10 @@
 from app import app, db
-from model import Expenses, Budget
+
+# ONE TIME USE, WILL NOT RUN THIS AGAIN
 
 #Create Expenses and Budget databases based on model defined in models file
 with app.app_context():
+    db.drop_all()
     db.create_all()
     
     
