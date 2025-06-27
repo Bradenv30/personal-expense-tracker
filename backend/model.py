e277ba6142adc2eedba7558da46746941a3b6b94
Braden Vail <bradenvail30@gmail.com>
2025-06-27 16:40:42 -0400

    Refactor table models into global scope, add table relationships, enforce table constraints

--- backend/model.py
+++ backend/model.py
@@ -1,22 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import CheckConstraint, func
 from datetime import datetime
 
+db = SQLAlchemy()
 
 # Create tables for expenses and budget
-def init_models(db):
-    class User(db.Model):
+
+class User(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(50), nullable=False, unique=True)
-        password = db.Column(db.String(50), nullable=False)
+        password = db.Column(db.String, nullable=False)
         created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
         #OTM relationships
         expenses = db.relationship("Expenses", back_populates="user")
         budgets = db.relationship("Budget", back_populates="user")
         
-    class Expenses(db.Model):
+class Expenses(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+        budget_id = db.Column(db.Integer, db.ForeignKey("budget.id"), nullable=False)
         name = db.Column(db.String(30))
         amount = db.Column(db.Numeric(10,2), nullable=False)
         date = db.Column(db.Date)
@@ -25,6 +28,8 @@ def init_models(db):
         
         #Relationship to User
         user = db.relationship("User", back_populates="expenses")
+        #Relationship to Budget
+        budget = db.relationship("Budget", back_populates="expenses")
     
         __table_args__ = (
             CheckConstraint("amount >= 0", name="check_amount_not_negative"),
@@ -34,7 +39,7 @@ def init_models(db):
             )
         )
     
-    class Budget(db.Model):
+class Budget(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
         name = db.Column(db.String(30))
@@ -47,10 +52,12 @@ def init_models(db):
         
         #Relationship to User
         user = db.relationship("User", back_populates="budgets")
+        #Relationship to Expenses (Budget parent)
+        expenses = db.relationship("Expenses", back_populates="budget")
         
         __table_args__ = (
             CheckConstraint("amount >= 0", name="check_amount_not_negative"),
         )
     
-    return User, Expenses, Budget
+    
 
